4ffe9e10bedcb02ab854245c6c9b267130a8155f
thomas37000 <thomas.chalanson@sfr.fr>
2023-04-30 15:40:35 +0200

    create Footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -53,13 +53,13 @@ const Header = () => {
             >
               <li>
                 <a
-                  className='logo block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-white dark:focus:text-white lg:p-2 [&.active]:text-black/90'
+                  className='logo block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-white dark:focus:text-white lg:p-2 [&.active]:text-black/90 text-red-500'
                   href='/'
                   data-te-nav-link-ref
                   data-te-ripple-init
                   data-te-ripple-color='light'
                 >
-                  Sushi Nerd
+                  Sushi Geek
                 </a>
               </li>
               <li className='mb-4 lg:mb-0 lg:pr-2' data-te-nav-item-ref>
